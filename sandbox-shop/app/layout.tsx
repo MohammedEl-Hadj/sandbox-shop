e7a76581b3f8e90f293a2fc2a82c76764e393b4d
MohammedEl-Hadj <mohamed.el-hadj@computershare.co.uk>
2026-03-30 21:58:46 +0100

    Update layout.tsx

--- sandbox-shop/app/layout.tsx
+++ sandbox-shop/app/layout.tsx
@@ -28,10 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        {/* GTM loader (head snippet equivalent) */}
+      <head>
+        {/* GTM "head snippet" (loaded via next/script) */}
         <Script
-          id="gtm"
+          id="gtm-head"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html:
